5b6a1e6cfc60021e2df17343fdead7603e77772e
Oliver Friesen <oliver.friesen@gmail.com>
2018-04-29 18:15:08 +0200

    Block "docker install" created

---
+++ dockerinstall/dockerinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#install docker
+curl -sSL https://get.docker.com | sh
+
+#install portainer ?
+if [ "$1" == "Enable" ]
+then
+	docker volume create portainer_data
+	docker run -d -p $2:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer	
+fi
+
+#enable user pi to control docker?
+if [ "$3" == "Enable" ]
+then
+	usermod -aG docker pi
+fi
